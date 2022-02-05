11a5e29a317e43d4230477f564c18c347715ce39
zhuhaojie <zhuhaojie@huikeyun.com>
2022-02-05 10:04:36 +0800

    feat: 补充信息

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
-## Usage
+## Install
 
 ```shell script
-yarn add lazy-image
-``` 
+yarn add hero-lazy-image
+```
+
+## Usage
+
+- src: 图片真实地址，必填
+- className: 图片类名，非必填
+- defaultimg: 真实图片加载完之前显示的默认图片，非必填(一般是一个比较小的图片)
+
+```html
+<LazyImage
+  className="lazyImage"
+  src="https://img.kaikeba.com/30155111102202dcbz.png"
+  defaultimg="https://img.kaikeba.com/30155111102202dcbz.png"
+/>
+```
 
 ## Contributors
 
--- src/lib/LazyImage/LazyImage.stories.js
+++ src/lib/LazyImage/LazyImage.stories.js
@@ -6,12 +6,14 @@ import LazyImage from '.';
 
 export default {
   /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
   title: 'LazyImage',
   component: LazyImage,
 };
 
-export const Primary = () => <LazyImage src='https://img.kaikeba.com/30155111102202dcbz.png' />;
-Primary.storyName = 'I am the primary';
+export const Primary = () => (
+  <LazyImage src="https://img.kaikeba.com/30155111102202dcbz.png" />
+);
+Primary.storyName = 'I am the LazyImage';
